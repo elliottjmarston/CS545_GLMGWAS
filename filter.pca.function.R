cebba2c1469ef4a0ab7c70bbe73ffdb3a7f02943
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-18 23:07:31 -0700

    updated @return statement

--- filter.pca.function.R
+++ filter.pca.function.R
@@ -3,7 +3,7 @@
 #' @param PCA The output list from the prcomp() function.
 #' @param covs A matrix/data.frame of covariates (rows = individuals, columns = covariates)
 #' @param threshold A numerical value (0-1) indicating the maximum allowed correlation between any one covariate and any one PC
-#' @return A matrix of filtered PCs (not correlated with any covariates supplied by user)
+#' @return A large prcomp with PCs correlated with covariates filtered out
 
 
 filter.pca <- function(PCA, covs, threshold=0.2){
